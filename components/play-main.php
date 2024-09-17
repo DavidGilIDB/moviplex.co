23dfec1ab6e884fcadd26a8d2682580b979ae2fa
davidIdb <david.gil@idbmobile.com>
2024-09-17 11:56:32 +0200

    add main templates

---
+++ components/play-main.php
@@ -0,0 +1,73 @@
+<div class="header-title category header-blue">
+      <h1>
+        <img
+          src="assets/icon-game.svg"
+          alt="logo"
+        />
+        Play
+      </h1>
+    </div>
+    <div class="latestUpdate bckPlay">
+      <h2 class="latestUpdate_title">Latest Updates</h2>
+      <div class="swiper swiper-initialized swiper-horizontal mySwiper swiper-backface-hidden">
+        <div class="swiper-wrapper" style="transform: translate3d(0px, 0px, 0px)">
+
+        <?php foreach ($recents as $app) { ?>
+            <div class="swiper-slide swiper-slide-active" style="width: 255.2px; margin-right: 30px">
+            <a href="#"><img src="<?= STORAGEURL . $app->image; ?>" /></a>
+          </div>
+        <?php } ?>
+        </div>
+        <div class="swiper-button-prev swiper-button-disabled"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal">
+          <span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span
+          ><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span
+          ><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span
+          ><span class="swiper-pagination-bullet"></span>
+        </div>
+      </div>
+      <a class="section-btn mt-4 d-block m-auto btnBlue" href="#/games/action-games">See All</a>
+    </div>
+    <div class="section-title text-center mb-0 pb-0 container">
+      <h1>Our Games &amp; Apps</h1>
+      <p class="categoriesSub">
+        Immerse yourself in a world of fun and excitement with our wide selection of cutting-edge games. From
+        <span class="blue"> thrilling adventures</span> to challenging <span class="blue">strategy games</span>.
+      </p>
+    </div>
+    <div class="main-sections">
+      <div>
+        <div id="topArticlesSection">
+          <div class="d-flex justify-content-center flex-column">
+            <div class="topArticlesTitle d-flex justify-content-between align-items-center">
+              <h2>Top Articles</h2>
+              <a href="#/articles/gaming-tips">See All</a>
+            </div>
+            <div class="container d-flex gap-4 justify-content-center mt-5 articles-frame">
+
+            <?php 
+            for ($article=0; $article < 4; $article++) { ?>
+                 <div class="article-card">
+                <picture
+                  ><img
+                    src="<?= STORAGEURL . $top[$article]->image; ?>"
+                    alt="Fortnite has become free on all platforms"
+                /></picture>
+                <div class="article-content p-4">
+                  <h5><?= $top[$article]->name; ?></h5>
+                  <div class="excerp">
+                    <p>
+                    <?= $top[$article]->description; ?>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <?php
+            }
+            ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
---
+++ components/watch-main.php
@@ -0,0 +1,61 @@
+<div class="header-title category header-red">
+  <h1>
+    <img
+      src="assets/icon-watch.svg"
+      alt="logo" />
+    Watch
+  </h1>
+</div>
+<div class="latestUpdate bckPlay">
+  <h2 class="latestUpdate_title">Latest Updates</h2>
+  <div class="swiper swiper-initialized swiper-horizontal mySwiper swiper-backface-hidden">
+    <div class="swiper-wrapper" style="transform: translate3d(0px, 0px, 0px)">
+
+      <?php foreach ($recents as $app) { ?>
+        <div class="swiper-slide swiper-slide-active" style="width: 255.2px; margin-right: 30px">
+          <a href="#"><img src="<?= 'assets/images/films/cover/' . $app->image; ?>" /></a>
+        </div>
+      <?php } ?>
+    </div>
+    <div class="swiper-button-prev swiper-button-disabled"></div>
+    <div class="swiper-button-next"></div>
+    <div class="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets swiper-pagination-horizontal">
+      <span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span>
+    </div>
+  </div>
+  <a class="section-btn mt-4 d-block m-auto btnRed" href="#/games/action-games">See All</a>
+</div>
+<div class="section-title text-center mb-0 pb-0 container">
+  <h1>Our movies, TV shows, documentaries...</h1>
+  <p class="categoriesSub">Explore our learning section, where you will find a variety of <span class="red"> courses </span> and <span class="red">tutorials</span> on a wide range of topics. From foreign languages to advanced technological skills.</p>
+</div>
+<div class="main-sections">
+  <a class="category-card red " href="/watch/comedy">
+    <div class="category-card-content">
+      <h3> Comedy</h3>
+      <p>Have a laugh with our selection of comedies</p>
+    </div>
+    <img class="category-card-image" src="assets/watch-comedy.png" alt="Games">
+  </a><a class="category-card red " href="/watch/thriller">
+    <div class="category-card-content">
+      <h3> Thriller</h3>
+      <p>Be frightened by our horror films and cult thrillers</p>
+    </div><img class="category-card-image" src="assets/watch-thriller.png" alt="Games">
+  </a><a class="category-card red " href="/watch/documentary">
+    <div class="category-card-content">
+      <h3> Documentaries</h3>
+      <p>Discover everything about the most popular characters</p>
+    </div><img class="category-card-image" src="assets/watch-docs.png" alt="Games">
+  </a><a class="category-card red " href="/watch/drama">
+    <div class="category-card-content">
+      <h3> Drama</h3>
+      <p>Great stories that will touch you in your heart</p>
+    </div><img class="category-card-image" src="assets/watch-drama.png" alt="Games">
+  </a><a class="category-card red " href="/watch/kids">
+    <div class="category-card-content">
+      <h3> Kids</h3>
+      <p>The best children's content to entertain your kids</p>
+    </div><img class="category-card-image" src="assets/watch-kids.png" alt="Games">
+  </a>
+  <div></div>
+</div>
